8129dae1d1006dcb6c6827fa1f87a50c8082a89a
miguelchacon8 <83428518+miguelchacon8@users.noreply.github.com>
2022-10-25 11:12:23 -0600

    final

---
+++ oscilador.c
@@ -0,0 +1,26 @@
+/*
+ * File:   oscilador.c
+ * Author: pablo
+ *
+ * Created on October 21, 2022, 9:02 AM
+ */
+
+#include "oscilador.h"
+
+
+//******************************************************************************
+// Función para configurar PWM
+//******************************************************************************
+void setupINTOSC(uint8_t IRCF){
+    
+    
+    if(IRCF == 7){
+        OSCCONbits.IRCF = 0b111;       // 8 MHz
+    }
+    if(IRCF == 6){
+        OSCCONbits.IRCF = 0b110;       // 4 MHz
+    }
+    
+    OSCCONbits.SCS = 1;
+}
+
---
+++ oscilador.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef OSCILADOR_H
+#define	OSCILADOR_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include <stdint.h>
+// TODO Insert appropriate #include <>
+
+
+// TODO Insert declarations
+void setupINTOSC(uint8_t IRCF);
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
